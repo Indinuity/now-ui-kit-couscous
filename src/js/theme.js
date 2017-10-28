656006246c242ff119339f7c69542cda783494c9
Jesse Badwal <jbadwal@indinuity.com>
2017-10-28 16:55:30 -0400

    updated template with carets

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,10 +6,10 @@
     $('.nav-item ul', ctx.closest('.menu'))
     .slideUp()
     .closest('.nav-item')
-    .find(".fa-caret-up")
-    .toggleClass("fa-caret-down fa-caret-up");
+    .find(".arrows-1_minimal-up")
+    .toggleClass("arrows-1_minimal-up arrows-1_minimal-down");
 
-    ctx.next().is(':visible') || ctx.next().slideDown().parent().find('.pull-right').toggleClass('fa-caret-down fa-caret-up');
+    ctx.next().is(':visible') || ctx.next().slideDown().parent().find('.float-right').toggleClass('arrows-1_minimal-down arrows-1_minimal-up');
     
     e.stopPropagation();
   })
