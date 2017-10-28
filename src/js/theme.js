49d2db343fdf5e67c9de2ee74ca681d0d9ce6d11
Jesse Badwal <jbadwal@indinuity.com>
2017-10-28 17:07:23 -0400

    updated js

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,12 +1,14 @@
 (function($) {
 
   $('body')
-  .on('click', 'nav.menu .nav-item > a', function(e) {
+  .on('click', 'nav.menu > .nav-item > a', function(e) {
     var ctx = $(this);
+    
+    // 
     $('.nav-item ul', ctx.closest('.menu'))
     .slideUp()
     .closest('.nav-item')
-    .find(".arrows-1_minimal-up,.arrows-1_minimal-down")
+    .find(".arrows-1_minimal-down")
     .toggleClass("arrows-1_minimal-up arrows-1_minimal-down");
 
     ctx.next().is(':visible') || ctx.next().slideDown().parent().find('.float-right').toggleClass('arrows-1_minimal-down arrows-1_minimal-up');
